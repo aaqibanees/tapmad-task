6ba3a616e3649ca63c7c233940bc9b7b2a1645b0
Aaqib Anees <aaqibanees@hotmail.co.uk>
2023-09-19 04:40:08 +0500

    deployed

--- src/index.js
+++ src/index.js
@@ -5,10 +5,8 @@ import App from './App';
 import {BrowserRouter} from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-const getBasename = path => path.substr(0, path.lastIndexOf('/'));
-
 root.render(
-    <BrowserRouter basename={getBasename(window.location.pathname)}>
+    <BrowserRouter basename={'/tapmad-task'}>
         <App />
     </BrowserRouter>
 );
