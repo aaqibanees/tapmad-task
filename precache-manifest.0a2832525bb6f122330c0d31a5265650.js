854d31fdde597a604113a61862ecf884c84610eb
Aaqib Anees <aaqibanees@hotmail.co.uk>
2023-09-18 19:02:29 +0500

    Updates

---
+++ precache-manifest.0a2832525bb6f122330c0d31a5265650.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c38c882e5639d7da6f7a",
+    "url": "/tapmad-task/static/css/main.733a1e7e.chunk.css"
+  },
+  {
+    "revision": "c38c882e5639d7da6f7a",
+    "url": "/tapmad-task/static/js/main.c38c882e.chunk.js"
+  },
+  {
+    "revision": "092946de2da1991820e7",
+    "url": "/tapmad-task/static/js/1.092946de.chunk.js"
+  },
+  {
+    "revision": "002e02c7e44777728c01",
+    "url": "/tapmad-task/static/js/runtime~main.002e02c7.js"
+  },
+  {
+    "revision": "cddae215c3a0354b3de2bbce19a15112",
+    "url": "/tapmad-task/index.html"
+  }
+];
